5e577f6bb4792d972f09001c5b61f1de1dde7880
Bunyodbek <93827769+BunyodbekNasriddinov@users.noreply.github.com>
2023-06-17 05:55:28 -0700

    aside started

--- src/components/aside/aside.jsx
+++ src/components/aside/aside.jsx
@@ -1,9 +1,29 @@
 import styles from './aside.module.css';
+import { NavLink } from 'react-router-dom';
 
 const Aside = () => {
 	return (
 		<div className={styles.aside}>
-			<div className='w-100 shadow'>asas</div>
+			<div className='w-100 shadow'>
+				<div className={styles.sidebar}></div>
+				<ul className={styles.sidebar__list}>
+					<li className={styles.sidebar__item}>
+						<NavLink activeClassName={styles.active} className={styles.sidebar__link}  to={'#'} >Xisobotlar</NavLink>
+					</li>
+					<li className={styles.sidebar__item}>
+						<NavLink activeClassName={styles.active} className={styles.sidebar__link} to={'#'} >O’quvchilar</NavLink>
+					</li>
+					<li className={styles.sidebar__item}>
+						<NavLink activeClassName={styles.active} className={styles.sidebar__link} to={'#'} >Guruhlar</NavLink>
+					</li>
+					<li className={styles.sidebar__item}>
+						<NavLink activeClassName={styles.active} className={styles.sidebar__link} to={'#'} >To’lovlar</NavLink>
+					</li>
+					<li className={styles.sidebar__item}>
+						<NavLink activeClassName={styles.active} className={styles.sidebar__link} to={'#'} >Davomat</NavLink>
+					</li>
+				</ul>
+			</div>
 		</div>
 	);
 };
