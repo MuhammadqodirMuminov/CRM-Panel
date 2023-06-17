0b7fd6331d20148213307562a2247447e70cad26
Bunyodbek <93827769+BunyodbekNasriddinov@users.noreply.github.com>
2023-06-17 06:13:38 -0700

    aside postion relative

--- src/components/aside/aside.jsx
+++ src/components/aside/aside.jsx
@@ -3,26 +3,28 @@ import { NavLink } from 'react-router-dom';
 
 const Aside = () => {
 	return (
-		<div className={styles.aside}>
-			<div className='w-100 shadow'>
-				<div className={styles.sidebar}></div>
-				<ul className={styles.sidebar__list}>
-					<li className={styles.sidebar__item}>
-						<NavLink activeClassName={styles.active} className={styles.sidebar__link}  to={'#'} >Xisobotlar</NavLink>
-					</li>
-					<li className={styles.sidebar__item}>
-						<NavLink activeClassName={styles.active} className={styles.sidebar__link} to={'#'} >O’quvchilar</NavLink>
-					</li>
-					<li className={styles.sidebar__item}>
-						<NavLink activeClassName={styles.active} className={styles.sidebar__link} to={'#'} >Guruhlar</NavLink>
-					</li>
-					<li className={styles.sidebar__item}>
-						<NavLink activeClassName={styles.active} className={styles.sidebar__link} to={'#'} >To’lovlar</NavLink>
-					</li>
-					<li className={styles.sidebar__item}>
-						<NavLink activeClassName={styles.active} className={styles.sidebar__link} to={'#'} >Davomat</NavLink>
-					</li>
-				</ul>
+		<div className={styles.relative}>
+			<div className={styles.aside}>
+				<div className='w-100 shadow'>
+					<div className={styles.sidebar}></div>
+					<ul className={styles.sidebar__list}>
+						<li className={styles.sidebar__item}>
+							<NavLink activeClassName={styles.active} className={styles.sidebar__link}  to={'#'} >Xisobotlar</NavLink>
+						</li>
+						<li className={styles.sidebar__item}>
+							<NavLink activeClassName={styles.active} className={styles.sidebar__link} to={'#'} >O’quvchilar</NavLink>
+						</li>
+						<li className={styles.sidebar__item}>
+							<NavLink activeClassName={styles.active} className={styles.sidebar__link} to={'#'} >Guruhlar</NavLink>
+						</li>
+						<li className={styles.sidebar__item}>
+							<NavLink activeClassName={styles.active} className={styles.sidebar__link} to={'#'} >To’lovlar</NavLink>
+						</li>
+						<li className={styles.sidebar__item}>
+							<NavLink activeClassName={styles.active} className={styles.sidebar__link} to={'#'} >Davomat</NavLink>
+						</li>
+					</ul>
+				</div>
 			</div>
 		</div>
 	);
