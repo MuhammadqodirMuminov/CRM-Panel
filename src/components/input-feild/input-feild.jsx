66e5703384279549d3c37a88ad4f90ea644c774a
muhammadqodirmuminov <muminovmuhammadqodir2@gmail.com>
2023-06-19 14:40:52 +0500

    formik validations and table delete

--- src/components/input-feild/input-feild.jsx
+++ src/components/input-feild/input-feild.jsx
@@ -1,22 +1,23 @@
 /* eslint-disable react/prop-types */
-// import { useField } from 'formik';
+import { useField } from 'formik';
 import styles from './input-feild.module.css';
 
 const InputFeild = ({ label, type = 'text', width = '320px', ...props }) => {
-	// const [field, meta] = useField(props);
+	const [field, meta] = useField(props);
 
 	return (
-		<div  style={{ width: width }} className={styles.feild}>
-			<label className={styles.label} htmlFor={props.id}>{label}</label>
+		<div style={{ width: width }} className={styles.feild}>
+			<label className={styles.label} htmlFor={props.id}>
+				{label}
+			</label>
 			<input
 				type={type}
 				id={props.id}
-				className={styles.input}
-				// className={meta?.error ? styles.error : styles.input}
-				// {...field}
+				className={meta?.error && meta?.touched ? styles.error : styles.input}
+				{...field}
 				{...props}
 			/>
-			{/* {meta?.error ? <span className={styles.errorSpan}>{meta.error}</span> : null} */}
+			{meta?.error && meta?.touched? <span className={styles.errorSpan}>{meta.error}</span> : null}
 		</div>
 	);
 };
--- src/schemas/index.js
+++ src/schemas/index.js
@@ -1,6 +1,7 @@
 import * as yup from 'yup';
 
 const passwordRules = /^(?=.*[A-Z])(?=.*\d).{5,}$/;
+const phoneRules = /^998\d{9}$/;
 
 export const registerSchema = yup.object().shape({
 	username: yup.string().required('username is required'),
@@ -10,4 +11,11 @@ export const registerSchema = yup.object().shape({
 		.min(5)
 		.matches(passwordRules, { message: 'password must  one uppercase element' })
 		.required('password is required'),
+});
+
+export const studentSchema = yup.object().shape({
+	student_name: yup.string().required('student name is required'),
+	student_phone: yup.string().matches(phoneRules).required('student phone is required'),
+	student_family_name: yup.string().required('student family name is required'),
+	student_family_phone: yup.string().matches(phoneRules).required('student phone is required'),
 });
